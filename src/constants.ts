433c6151941d2a4eed599f565cf1b5d406247367
parrr.wei.geng <parrr.wei.geng@gmail.com>
2024-01-26 21:49:52 +0100

    chore: set up local dev and test

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
-export const API_URL =
-  process.env.REACT_APP_API_URL || "https://germany-ranking.herokuapp.com";
+// export const API_URL =
+//   process.env.REACT_APP_API_URL || "https://germany-ranking.herokuapp.com";
+
+export const API_URL = "https://germany-ranking.herokuapp.com";
