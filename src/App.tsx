c9e40533401a0ee364c0d39baa48fae554162a3d
Oliver Iyer <oliver.iyer@doctolib.com>
2024-02-02 08:35:22 +0100

    remove router, use query

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import GermanRankings from "./pages/GermanRankings";
 import AustrianRankings from "./pages/AustrianRankings";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<GermanRankings />} />
-      <Route path="/austria" element={<AustrianRankings />} />
-    </Routes>
-  );
+  const query = new URLSearchParams(window.location.search);
+  const country = query.get("country");
+
+  if (country === "austria") {
+    return <AustrianRankings />;
+  }
+
+  return <GermanRankings />;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
